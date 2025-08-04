a8a9c50c4807feef142d6da0469eb9b766b0444b
Nikole <codecute9@gmail.com>
2025-08-04 14:35:45 -0700

    Day3. Working Scoreboard and new screen!

--- app.py
+++ app.py
@@ -1,20 +1,39 @@
 from flask import Flask, redirect, url_for, request, render_template
 app = Flask(__name__)
+score = 0
 
-
-
+# First gamescreen
 @app.route("/gamescreen1")
 def index2():
-    if 
-    return render_template("gamescreen1.html", score_response = "You are right!")
+        global score 
+        print(score)
+        return render_template("gamescreen1.html", score_response = "What Image is real?", score_board = score)
 
+# This is gamescreen 2
+@app.route("/screen2")
+def indexQ2():
+    global score 
+    print(score)
+    return render_template("gamescreen1.html", score_response = "You are right!", score_board = score)
+    
+# Right Screen   
+@app.route("/rightscreen")
+def indexYes():
+        global score 
+        score = score + 1
+        print(score)
+        return render_template("rightscreen.html", score_response = "You are right!", score_board = score )
+
+# Wrong Screen
 @app.route("/wrongscreen")
-def index3():
+def indexNo():
      return render_template("wrongscreen.html")
 
-
+# Homescreen
 @app.route("/", methods = ["POST", "GET"])
 def index():
+        global score
+        score = 0
         return render_template("index.html")
 
 # main driver function
