9b420a46d45f3a91432135575028ab89904da8c8
Nikole <codecute9@gmail.com>
2025-08-06 14:49:18 -0700

    Day5

--- app.py
+++ app.py
@@ -14,9 +14,19 @@ def index2():
 def indexQ2():
     global score 
     print(score)
-    return render_template("gamescreen1.html", score_response = "Keep going, what image is real?", score_board = score)
-    
-# Right Screen   
+    return render_template("gamescreen2.html", score_response = "Keep going, what image is real?", score_board = score)
+
+# This is gamescreen 3
+@app.route("/screen3")
+def indexQ3():
+    global score 
+    print(score)
+    return render_template("gamescreen3.html", score_response = "Nice! Lets continue", score_board = score)
+
+# _______________________________________________________
+
+
+# Right Screen 1
 @app.route("/rightscreen")
 def indexYes():
         global score 
@@ -24,10 +34,32 @@ def indexYes():
         print(score)
         return render_template("rightscreen.html", score_response = "You are right!", score_board = score )
 
-# Wrong Screen
+# Right Screen 2
+@app.route("/rightscreen2")
+def indexYes1():
+        global score 
+        score = score + 1
+        print(score)
+        return render_template("rightscreen2.html", score_response = "You are right!", score_board = score )
+
+# Right Screen 3
+@app.route("/rightscreen3")
+def indexYes2():
+        global score 
+        score = score + 1
+        print(score)
+        return render_template("rightscreen.html", score_response = "You are right!", score_board = score )
+
+
+# _______________________________________________________
+
+# Wrong Screen 1
 @app.route("/wrongscreen")
 def indexNo():
      return render_template("wrongscreen.html")
+
+
+# _______________________________________________________
 
 # Homescreen
 @app.route("/", methods = ["POST", "GET"])
@@ -35,6 +67,8 @@ def index():
         global score
         score = 0
         return render_template("index.html")
+
+
 
 # main driver function
 if __name__ == '__main__':
